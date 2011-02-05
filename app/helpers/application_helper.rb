e29131cec651a1aab49d598fb7dfb96c58598de8
Vladimir Adamić <vladimir.adamic@gmail.com>
2011-02-05 18:08:50 +0100

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  def logo
+	logo = image_tag("logo.png", :alt => "Sample App", :class => "round")  
+  end
+
+
+
   # Return a title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users  
+  
+  #get "users/new"
 
   match '/signup', :to => 'users#new'
 
